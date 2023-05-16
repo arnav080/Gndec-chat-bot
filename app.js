e415cfd9d84d906cff41cf844c954205e79b15a9
DaveyDark <devesh97531@gmail.com>
2023-05-17 01:56:13 +0530

    Added chat

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const api_url = 'http://localhost:5000'
+
 let toggle_btn = document.getElementById('access-button');
       let faqpage = document.getElementById('popup');
       let hidden = true;
@@ -45,34 +47,59 @@ function animate(){
 }
 // trying to make a new faq div for every coming from the api
 
-fetch('http://localhost:5000/get-all')
+fetch(api_url + '/get-all')
 .then(response => response.json())
 .then(data =>  {
     data.faqs.forEach(faq => {
-        console.log(`Question: ${faq.question}`);
-        console.log(`Answer: ${faq.answer}`);
-        const container = document.querySelector('.message-content');
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class', 'wrapper');
-        const toggle = document.createElement('button');
-        toggle.setAttribute('class', 'toggle');
-        const questionp = document.createElement('p');
-        questionp.setAttribute('class', 'question-p');
-        questionp.textContent = faq.question;
-        const questioni=document.createElement('i')
-        questioni.setAttribute('class','icon fa-solid fa-plus')
-        const answer=document.createElement('div')
-        answer.setAttribute('class','answer')
-        const answerp=document.createElement('p')
-        answerp.setAttribute('class','answer-p')
-        answerp.textContent=faq.answer
-        container.appendChild(wrapper);
-        wrapper.appendChild(toggle);
-        toggle.appendChild(questionp);
-        toggle.appendChild(questioni)
-        answer.appendChild(answerp)
-        wrapper.appendChild(answer)
+        createMessage(faq.question,faq.answer);
     })
 }).then(animate)
 
+function createMessage(ques, ans){
+    const container = document.querySelector('.message-content');
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'wrapper');
+    const toggle = document.createElement('button');
+    toggle.setAttribute('class', 'toggle');
+    const questionp = document.createElement('p');
+    questionp.setAttribute('class', 'question-p');
+    questionp.textContent = ques;
+    const questioni=document.createElement('i')
+    questioni.setAttribute('class','icon fa-solid fa-plus')
+    const answer=document.createElement('div')
+    answer.setAttribute('class','answer')
+    const answerp=document.createElement('p')
+    answerp.setAttribute('class','answer-p')
+    answerp.textContent=ans
+    container.appendChild(wrapper);
+    wrapper.appendChild(toggle);
+    toggle.appendChild(questionp);
+    toggle.appendChild(questioni)
+    answer.appendChild(answerp)
+    wrapper.appendChild(answer)
+}
+
+// add on click event to send button
+send_button = document.querySelector('.send_button');
+send_button.addEventListener('click', () => {
+    //remove exisitng messages
+    let wrapperParent = document.querySelector('.message-content');
+    while (wrapperParent.firstChild) {
+        wrapperParent.removeChild(wrapperParent.firstChild);
+    }
+
+    //add response received
+    let query = document.getElementById('query-input').value
+    fetch(api_url + '/query', {
+        method: 'POST',
+        body: JSON.stringify({ "query": query}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json()).then(data => {
+        data.forEach(pair => {
+            createMessage(pair.question,pair.answer)
+        })
+    }).then(animate)
+});
 
